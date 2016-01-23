74750026f54481a1fcd7c61f2a7ee95d45eaebad
Oskar Kvamme <oskar@kvamme.net>
2016-01-24 00:09:59 +0100

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,87 @@
+import gulp from 'gulp';
+import postcss from 'gulp-postcss';
+import sass from 'gulp-sass';
+import unmq from 'postcss-unmq';
+import runSequence from 'run-sequence';
+import del from 'del';
+import connect from 'gulp-connect';
+
+const formats = [
+  {
+    width: 1200,
+    height: 400
+  },
+  {
+    width: 600,
+    height: 200
+  }
+];
+
+//develop
+gulp.task('sass', () => {
+  return gulp.src('./sass/screen.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:livereload', () => {
+  return gulp.src('./sass/screen.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('sass:watch', () => {
+  return gulp.watch('./sass/**/*.scss', ['sass:livereload']);
+});
+
+gulp.task('html:watch', () => {
+  return gulp.watch('./index.html', () =>{
+    gulp.src('./index.html')
+    .pipe(connect.reload());
+  });
+});
+
+gulp.task('default', ['sass']);
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true,
+    port: 8888
+  });
+});
+
+gulp.task('serve', ['webserver', 'sass:watch', 'html:watch']);
+
+
+//build
+gulp.task('clean', function () {
+  return del([
+    'build/**/*'
+  ]);
+});
+
+gulp.task('movestatic', () => {
+  formats.forEach(format => {
+    const path = format.width + 'X' + format.height;
+    gulp.src('./index.html').pipe(gulp.dest('./build/' + path));
+  });
+});
+
+gulp.task('buildcss', () => {
+  formats.forEach(format => {
+    const processors = [
+      unmq({width : format.width, height: format.height})
+    ];
+
+    const path = format.width + 'X' + format.height + '/css';
+
+    gulp.src('./css/screen.css')
+      .pipe(postcss(processors))
+      .pipe(gulp.dest('./build/' + path));
+  });
+});
+
+gulp.task('build', () => {
+  return runSequence('clean', ['default', 'movestatic'], 'buildcss');
+});
