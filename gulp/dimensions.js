d18ec776f736317dc3b2258c3b47dcbf320bfc0a
Oskar Kvamme <oskar@kvamme.net>
2016-03-09 13:01:39 +0100

    added default view ports/breakpoints

--- gulp/dimensions.js
+++ gulp/dimensions.js
@@ -1,11 +1,39 @@
 //view port dimensions
 module.exports =[
-  {
-    width: 1200,
-    height: 400
-  },
-  {
-    width: 600,
-    height: 200
-  }
+    {
+      width: 180,
+      height: 500
+    },
+    {
+      width: 250,
+      height: 250
+    },
+    {
+      width: 300,
+      height: 250
+    },
+    {
+      width: 320,
+      height: 250
+    },
+    {
+      width: 468,
+      height: 300
+    },
+    {
+      width: 468,
+      height: 400
+    },
+    {
+      width: 580,
+      height: 500
+    },
+    {
+      width: 300,
+      height: 600
+    },
+    {
+      width: 980,
+      height: 300
+    }
 ];
