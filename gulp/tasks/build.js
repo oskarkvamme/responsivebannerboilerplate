6f561d618078c11de2099f7b67f33f5264a7b240
Oskar Kvamme <oskar@kvamme.net>
2016-03-14 22:49:04 +0100

    preview and inline script and styling

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,14 +1,34 @@
 var gulp = require('gulp');
+var inject = require('gulp-inject-string');
 var runSequence = require('run-sequence');
 var cssnano = require('gulp-cssnano');
 var zip = require('gulp-zip');
 var minify = require('gulp-minify');
+var fs = require('fs');
+var Pageres = require('pageres');
+var rename = require('gulp-rename');
 
 var config = require('../config');
+var bannerConfig = require('../../bannerConfig');
+
+var bannerSizes = bannerConfig.banners.map(function(banner){
+  return banner.width + 'x' + banner.height;
+});
+
+var zipTasks = bannerSizes.map(function(bannerSize){
+  return 'zip-' + bannerSize;
+});
 
 //build
-gulp.task('movestatic', function() {
-    return gulp.src('./index.html').pipe(gulp.dest(config.buildPath + '/'));
+gulp.task('buildhtml', function() {
+
+  var script = '<script>' + fs.readFileSync(config.buildPath + '/js/entry-min.js', "utf8") + '</script>';
+  var css = '<style>' + fs.readFileSync(config.buildPath + '/css/screen.css', "utf8") + '</style>';
+
+  return gulp.src(config.indexFile)
+        .pipe(inject.replace('<link rel="stylesheet" type="text/css" href="css/screen.css" />', css))
+        .pipe(inject.replace('<script src="js/entry.js" />', script))
+        .pipe(gulp.dest(config.buildPath + '/'));
 });
 
 gulp.task('buildfinalcss', function() {
@@ -23,12 +43,26 @@ gulp.task('buildfinaljs', function(){
     .pipe(gulp.dest(config.buildPath + '/js'))
 });
 
-gulp.task('zip', () => {
-    return gulp.src(config.buildPath + '/**/*')
-        .pipe(zip('banner.zip'))
-        .pipe(gulp.dest('dist'));
+bannerSizes.forEach(function(bannerSize){
+  gulp.task('zip-' + bannerSize, function() {
+      return gulp.src(config.buildPath + '/{index.html,' + bannerSize + '.png}')
+          .pipe(rename(function (path) {
+            if(path.extname === '.png'){
+              path.basename = 'preview';
+            }
+          }))
+          .pipe(zip('banner-' + bannerSize + '.zip'))
+          .pipe(gulp.dest('dist'));
+  });
+});
+
+gulp.task('screenshot', function (){
+  return new Pageres({delay: 2, filename: '<%= size %>'})
+    .src(config.buildPath + '/index.html', bannerSizes)
+    .dest(config.buildPath + '/')
+    .run()
 });
 
 gulp.task('build', function() {
-  return runSequence('clean-build-folder', 'sass:dist', 'webpack', 'movestatic', 'buildfinalcss', 'buildfinaljs', 'clean-dist-folder', 'zip');
+  return runSequence('clean-build-folder', 'sass:dist', 'webpack', 'buildfinalcss', 'buildfinaljs', 'buildhtml', 'clean-dist-folder', 'screenshot', zipTasks);
 });
