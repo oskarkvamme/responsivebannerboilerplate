02a32b6e984035e3a49e3b15d4cdb58cc58a1f93
Oskar Kvamme <oskar@kvamme.net>
2016-09-20 23:11:38 +0200

    current banner hard coded on build

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -19,18 +19,11 @@ var zipTasks = bannerSizes.map(function(bannerSize){
   return 'zip-' + bannerSize;
 });
 
-//build
-gulp.task('buildhtml', function() {
-
-  var script = '<script>' + fs.readFileSync(config.buildPath + '/js/entry-min.js', "utf8") + '</script>';
-  var css = '<style>' + fs.readFileSync(config.buildPath + '/css/screen.css', "utf8") + '</style>';
-
-  return gulp.src(config.indexFile)
-        .pipe(inject.replace('<link rel="stylesheet" type="text/css" href="css/screen.css" />', css))
-        .pipe(inject.replace('<script src="js/entry.js"></script>', script))
-        .pipe(gulp.dest(config.buildPath + '/'));
+var htmlTasks = bannerSizes.map(function(bannerSize){
+  return 'buildhtml-' + bannerSize;
 });
 
+//build
 gulp.task('buildfinalcss', function() {
   return gulp.src(config.compiledCss)
     .pipe(cssnano())
@@ -43,12 +36,47 @@ gulp.task('buildfinaljs', function(){
     .pipe(gulp.dest(config.buildPath + '/js'))
 });
 
+gulp.task('buildstandardhtml', function() {
+  var script = '<script>' + fs.readFileSync(config.buildPath + '/js/entry-min.js', "utf8") + '</script>';
+  var css = '<style>' + fs.readFileSync(config.buildPath + '/css/screen.css', "utf8") + '</style>';
+
+  return gulp.src(config.indexFile)
+        .pipe(inject.replace('<link rel="stylesheet" type="text/css" href="css/screen.css" />', css))
+        .pipe(inject.replace('<script src="js/entry.js"></script>', script))
+        .pipe(gulp.dest(config.buildPath + '/'));
+});
+
 bannerSizes.forEach(function(bannerSize){
+    gulp.task('buildhtml-' + bannerSize, function() {
+      var width = parseInt(bannerSize.split('x')[0], 10);
+      var height = parseInt(bannerSize.split('x')[1], 10);
+      var currentBanner = bannerConfig.banners.filter(function(banner) {
+          return banner.width === width && banner.height === height;
+      })[0];
+
+      var currentBannerScript = '<script>window.currentBanner=' + JSON.stringify(currentBanner) + ';</script>';
+      var script = '<script>' + fs.readFileSync(config.buildPath + '/js/entry-min.js', "utf8") + '</script>';
+      var css = '<style>' + fs.readFileSync(config.buildPath + '/css/screen.css', "utf8") + '</style>';
+
+      return gulp.src(config.indexFile)
+            .pipe(inject.replace('<link rel="stylesheet" type="text/css" href="css/screen.css" />', css))
+            .pipe(inject.replace('<script src="js/entry.js"></script>', script))
+            .pipe(inject.replace('<script id="currentBanner"></script>', currentBannerScript))
+            .pipe(rename(function (path) {
+                path.basename = 'index' + bannerSize;
+            }))
+            .pipe(gulp.dest(config.buildPath + '/'));
+    });
+
+
   gulp.task('zip-' + bannerSize, function() {
-      return gulp.src(config.buildPath + '/{index.html,' + bannerSize + '.png}')
+      return gulp.src(config.buildPath + '/{index' + bannerSize + '.html,' + bannerSize + '.png}')
           .pipe(rename(function (path) {
             if(path.extname === '.png'){
               path.basename = 'preview';
+            }
+            if (path.extname === '.html') {
+                path.basename = 'index';
             }
           }))
           .pipe(zip('banner-' + bannerSize + '.zip'))
@@ -64,5 +92,5 @@ gulp.task('screenshot', function (){
 });
 
 gulp.task('build', function() {
-  return runSequence('clean-build-folder', 'sass:dist', 'webpack', 'buildfinalcss', 'buildfinaljs', 'buildhtml', 'clean-dist-folder', 'screenshot', zipTasks);
+  return runSequence('clean-build-folder', 'sass:dist', 'webpack', 'buildfinalcss', 'buildfinaljs', htmlTasks, 'buildstandardhtml', 'clean-dist-folder', 'screenshot', zipTasks);
 });
--- scripts/getBanner.js
+++ scripts/getBanner.js
@@ -1,13 +1,22 @@
+/* globals window */
 var bannerConfig = require('../bannerConfig');
 
 module.exports = function(){
+
+  if (window.currentBanner) {
+      return window.currentBanner;
+  }
+
+  const margin = 2;
+
   var currentBanner = bannerConfig.banners.filter(function(banner){
-      return window.innerWidth === banner.width && window.innerHeight === banner.height;
+      return window.innerWidth < (banner.width + margin) && window.innerWidth > (banner.width - margin)
+        && window.innerHeight < (banner.height + margin) && window.innerHeight > (banner.height - margin);
   });
 
-  if(!currentBanner.length){
+  if (!currentBanner.length) {
       currentBanner = bannerConfig.defaultBanner;
-  }else{
+  } else {
       currentBanner = currentBanner[0];
   }
 
